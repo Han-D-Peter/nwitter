3055f004da0b95d40ee5feb272778ab7488c4ea5
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-11-30 17:48:05 +0900

    5.1

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { authService } from "../fbase";
+import { authService, dbService } from "../fbase";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async event => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({ displayName: newDisplayName });
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
